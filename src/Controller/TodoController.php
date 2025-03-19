5ae9bc8d6362a2e8b62f1f18dc3b83cf9b616775
Felix <felixbecker21@yahoo.com>
2025-03-19 21:45:02 +0100

    Entfernen-Button hinzugefügt+Route auf POST engeschränkt(SICHERHEIT)+refactor

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,35 +2,48 @@
 
 namespace App\Controller;
 
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\HttpFoundation\Request;
-    use Symfony\Component\HttpFoundation\Session\SessionInterface;
-    use Symfony\Component\Routing\Annotation\Route;
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-    class TodoController extends AbstractController
+class TodoController extends AbstractController
+{
+    #[Route('/todoapp', name: 'todoapp', methods: ['POST'])]   
+    public function createTodo(Request $request, SessionInterface $session): Response
     {
-        
-        #[Route('/todoapp', name: 'todoapp')]   
+        if(!$session->has('todos')) {
+            $session->set('todos', []);
+        }
+        $todos = $session->get('todos');
 
-        public function index(Request $request, SessionInterface $session): Response
-        {
-            if(!$session->has('todos')) {
-                $session->set('todos', []);
+        if($request->isMethod('POST')) {
+            $newTodo = $request->request->get('todos');
+            if($newTodo) {
+                $todos[] = $newTodo;
+                $session->set('todos', $todos);
             }
-            $todos = $session->get('todos');
+        }
+        
+        return $this->render('todo/todoapp.html.twig', [
+            'todos' => $todos,
+        ]);  
+    }
 
-            if($request->isMethod('POST')) {
-                $newTodo = $request->request->get('todos');
-                if($newTodo) {
-                    $todos[] = $newTodo;
-                    $session->set('todos', $todos);
+    #[Route('/deleteTodo', name: 'deleteTodo', methods: ['POST'])]
+    public function removeTodo(Request $request, SessionInterface $session): Response
+    {
+        if ($request->isMethod('POST')) {
+            $todoIndex = $request->request->get('index');
+            if ($session->has('todos')) {
+                $todos = $session->get('todos');
+                if (isset($todos[$todoIndex])) {
+                    unset($todos[$todoIndex]);
+                    $session->set('todos', array_values($todos));
                 }
             }
-            
-            return $this->render('todo/todoapp.html.twig', [
-                'todos' => $todos,
-            ]);
-            
         }
+        return $this->redirectToRoute('todoapp');
     }
+}
