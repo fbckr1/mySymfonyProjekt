78f399773a051d20f27f4195d69595ceea8090f4
Felix <felixbecker21@yahoo.com>
2025-03-19 20:40:34 +0100

    Todos werden aufgelistet und neue können hinzugefügt werden

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
-    use symfony\Component\HttpFoundation\Response;
-    use symfony\Component\Routing\Annotation\Route;
-    use symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Session\SessionInterface;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
     class TodoController extends AbstractController
     {
         
         #[Route('/todoapp', name: 'todoapp')]   
 
-        public function index(): Response
+        public function index(Request $request, SessionInterface $session): Response
         {
-            $todos = [];
+            if(!$session->has('todos')) {
+                $session->set('todos', []);
+            }
+            $todos = $session->get('todos');
+
+            if($request->isMethod('POST')) {
+                $newTodo = $request->request->get('todos');
+                if($newTodo) {
+                    $todos[] = $newTodo;
+                    $session->set('todos', $todos);
+                }
+            }
+            
             return $this->render('todo/todoapp.html.twig', [
                 'todos' => $todos,
             ]);
